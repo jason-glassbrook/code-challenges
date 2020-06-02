25b8d718a3763c4102c2cdf1ce10670810992242
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-06-02 17:11:24 -0400

    leetcode: add longest_substring_without_repeating_characters.py

---
+++ leetcode.com/longest_substring_without_repeating_characters.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        w__left = 0
+        w__chars = dict()
+        answer = 0
+
+        for (w__right, c) in enumerate(s):
+
+            if c in w__chars and w__chars[c] >= w__left:
+
+                w__left = w__chars[c] + 1
+
+            w__chars[c] = w__right
+            answer = max(answer, w__right - w__left + 1)
+
+        return answer
+
+    def lengthOfLongestSubstring__submission_1(self, s: str) -> int:
+
+        s__len = len(s)
+        w__left, w__right = 0, 0
+        w__chars = set()
+        answer = 0
+
+        while w__left < s__len and w__right < s__len:
+
+            if s[w__right] not in w__chars:
+
+                w__chars.add(s[w__right])
+                w__right += 1
+                answer = max(answer, w__right - w__left)
+
+            else:
+
+                w__chars.remove(s[w__left])
+                w__left += 1
+
+        return answer
