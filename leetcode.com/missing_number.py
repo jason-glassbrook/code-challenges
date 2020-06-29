5508622c0b1b3bf05c9c5c3c097cfcd52e19e8e4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-06-29 15:26:53 -0400

    leetcode.com/missing_number.py: setup

--- leetcode.com/missing_number.py
+++ leetcode.com/missing_number.py
@@ -1,1 +1,61 @@
 #!python3
+
+from typing import List
+
+
+class Solution:
+
+    def missingNumber(self, nums: List[int]) -> int:
+
+        return self.missingNumber__brute_force(nums)
+
+    def missingNumber__brute_force(self, nums: List[int]) -> int:
+
+        if not nums:
+            return None
+
+        return 0
+
+    def missingNumber__hash_set(self, nums: List[int]) -> int:
+
+        if not nums:
+            return None
+
+        return 0
+
+    def missingNumber__xor(self, nums: List[int]) -> int:
+
+        if not nums:
+            return None
+
+        return 0
+
+    def missingNumber__sum(self, nums: List[int]) -> int:
+
+        if not nums:
+            return None
+
+        return 0
+
+
+if __name__ == "__main__":
+
+    tests = [{
+        "args": ([3, 0, 1],),
+        "answer": 2,
+    }, {
+        "args": ([9, 6, 4, 2, 3, 5, 7, 0, 1],),
+        "answer": 8,
+    }]
+
+    solution = Solution()
+
+    for test in tests:
+
+        args = test["args"]
+        answer = test["answer"]
+
+        print("\n--- test case ---\n")
+        print(args)
+        print("expected:", answer)
+        print("returned:", solution.missingNumber(*args))
