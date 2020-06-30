ce0490a3f9ff0145d3e779b1f03908c0f3704099
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-06-30 13:56:00 -0400

    l~c~/s~o~t~i~.py: solve with bitwise operations

--- leetcode.com/sum_of_two_integers.py
+++ leetcode.com/sum_of_two_integers.py
@@ -28,11 +28,32 @@ class Solution:
 
     def getSum(self, a: int, b: int) -> int:
 
-        return self.getSum__cheating(a, b)
+        return self.getSum__bitwise_operations(a, b)
 
     def getSum__cheating(self, a: int, b: int) -> int:
 
         return sum((a, b))    # This is cheating!
+
+    def getSum__bitwise_operations(self, a: int, b: int) -> int:
+
+        total = max(a, b)    # Initial "total"
+        carry = min(a, b)    # Initial "carry" -- min of a, b for fewer iterations
+
+        # Fiddle with bits while bits still need to be carried.
+        while (carry > 0):
+
+            # Remember the previous state.
+            prev__total = total
+            prev__carry = carry
+
+            # Compute the next state.
+            # -   `a XOR b` gives the bits that differ -- these don't need to be carried.
+            # -   `a AND b` gives the bits that are both 1 -- these need to be carried.
+            # -   `(a AND b) LSHIFT 1` carries the bits whose sum overflow the range 0 to 1.
+            total = (prev__total ^ prev__carry)
+            carry = (prev__total & prev__carry) << 1
+
+        return total
 
 
 if __name__ == "__main__":
