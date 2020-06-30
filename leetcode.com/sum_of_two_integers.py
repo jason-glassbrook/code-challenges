98d8bcf092a27c754503a06debab51c7032d31cf
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-06-30 14:03:23 -0400

    l~c~/s~o~t~i~.py: solve recursively with bitwise operations

--- leetcode.com/sum_of_two_integers.py
+++ leetcode.com/sum_of_two_integers.py
@@ -28,13 +28,13 @@ class Solution:
 
     def getSum(self, a: int, b: int) -> int:
 
-        return self.getSum__bitwise_operations(a, b)
+        return self.getSum__bitwise_operations__recursive(a, b)
 
     def getSum__cheating(self, a: int, b: int) -> int:
 
         return sum((a, b))    # This is cheating!
 
-    def getSum__bitwise_operations(self, a: int, b: int) -> int:
+    def getSum__bitwise_operations__iterative(self, a: int, b: int) -> int:
 
         total = max(a, b)    # Initial "total"
         carry = min(a, b)    # Initial "carry" -- min of a, b for fewer iterations
@@ -54,6 +54,29 @@ class Solution:
             carry = (prev__total & prev__carry) << 1
 
         return total
+
+    def getSum__bitwise_operations__recursive(self, a: int, b: int) -> int:
+        """
+        Because why not?
+        """
+
+        def __next(total: int, carry: int) -> int:
+
+            if (carry > 0):
+
+                return __next(
+                    total=(total ^ carry),
+                    carry=((total & carry) << 1),
+                )
+
+            else:
+
+                return total
+
+        return __next(
+            total=max(a, b),    # Initial "total"
+            carry=min(a, b),    # Initial "carry" -- min of a, b for fewer iterations
+        )
 
 
 if __name__ == "__main__":
