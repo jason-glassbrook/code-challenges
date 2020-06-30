07ce157c6d5eb5371b66aaab9afad009cb341d52
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-06-30 13:18:18 -0400

    leetcode.com/sum_of_two_integers.py: setup

--- leetcode.com/sum_of_two_integers.py
+++ leetcode.com/sum_of_two_integers.py
@@ -1,1 +1,35 @@
 #!python3
+
+import unittest
+import random
+
+
+class TestSolution(unittest.TestCase):
+
+    def _run_solution(self, args):
+        return Solution().getSum(*args)
+
+    def test_sum(self):
+
+        a = random.randint(0, 0xFFFFFFFF)
+        b = random.randint(0, 0xFFFFFFFF)
+
+        args = (a, b)
+        answer = (a + b)
+
+        result = self._run_solution(args)
+
+        self.assertEqual(result, answer)
+
+        return
+
+
+class Solution:
+
+    def getSum(self, a: int, b: int) -> int:
+
+        return (a + b)    # This is cheating!
+
+
+if __name__ == "__main__":
+    unittest.main()
