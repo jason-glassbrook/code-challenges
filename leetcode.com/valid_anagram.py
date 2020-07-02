888e8754c08ddf329a06a1e981b585d74c03031f
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-02 18:06:07 -0400

    l~c~/v~a~.py: refac shared code, def types

--- leetcode.com/valid_anagram.py
+++ leetcode.com/valid_anagram.py
@@ -2,12 +2,32 @@
 
 ############################################################
 
+from typing import (
+    Callable,
+    Iterable,
+    Mapping,
+)
+from collections import (
+    Counter,
+    defaultdict as DefaultDict,
+)
+
+#--- Types ---#
+_Counter = Mapping[str, int]
+_CounterFun = Callable[[str], _Counter]
+_CompareFun = Callable[[_Counter, _Counter], bool]
+_SolutionFun = Callable[[str, str], bool]
+
 
 class Solution:
 
     def isAnagram(self, s1: str, s2: str) -> bool:
 
-        return self.isAnagram__custom_counter__equals_operator(s1, s2)
+        return self.isAnagram__custom_counter__compare_keys(s1, s2)
+
+    ########################################
+    #   Algorithms
+    ########################################
 
     def isAnagram__python_counter__equals_operator(self, s1: str, s2: str) -> bool:
         """
@@ -18,56 +38,22 @@ class Solution:
         This is the easiest way to solve this problem that I can think of.
         """
 
-        from collections import Counter
+        return self._solve_with(
+            counter=self._counter__python_counter,
+            compare=self._compare__equals_operator,
+        )(s1, s2)
 
-        #---------------------------------------
-
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
-            return False
-
-        # And then it's just...
-        return (Counter(s1) == Counter(s2))
-
-    def isAnagram__python_counter__comparing_keys(self, s1: str, s2: str) -> bool:
+    def isAnagram__python_counter__compare_keys(self, s1: str, s2: str) -> bool:
         """
         Solves "valid anagram" problem...
         -   using Python's `collections.Counter`
         -   by iteratively comparing the counts of keys
         """
 
-        from collections import Counter
-
-        #---------------------------------------
-
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
-            return False
-
-        # Count the characters in `s1` and `s2`.
-        c1 = Counter(s1)
-        c2 = Counter(s2)
-
-        # If `c1` and `c2` don't have the same number of keys, then `s1` and `s2` can't be anagrams.
-        if len(c1) != len(c2):
-            return False
-
-        # Compare the counts of keys.
-        # -   Because `c1` and `c2` have the same number of keys,
-        #     they must share all letters (but not necessarily their counts)
-        #     or they have the same number of unique letters (but their letters are different).
-        for letter in c1.keys():
-            # Does `letter` occur in `s2`?
-            if letter in c2.keys():
-                # Does `letter` occur in `s1` and `s2` the same number of times?
-                if c1[letter] == c2[letter]:
-                    # 🥳 Move on to the next letter!
-                    continue
-
-            return False
-
-        # If we made it here, then `s1` and `s2` are anagrams!
-        return True
+        return self._solve_with(
+            counter=self._counter__python_counter,
+            compare=self._compare__compare_keys,
+        )(s1, s2)
 
     def isAnagram__python_defaultdict__equals_operator(self, s1: str, s2: str) -> bool:
         """
@@ -76,76 +62,22 @@ class Solution:
         -   using Python's equals operator `==`
         """
 
-        from collections import defaultdict as DefaultDict
+        return self._solve_with(
+            counter=self._counter__python_defaultdict,
+            compare=self._compare__equals_operator,
+        )(s1, s2)
 
-        # Create a dict counting the elements of an iterable.
-        def counter_dict(iterable):
-
-            counter = DefaultDict(int)
-
-            for item in iterable:
-                counter[item] += 1
-
-            return counter
-
-        #---------------------------------------
-
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
-            return False
-
-        # And then it's just...
-        return (counter_dict(s1) == counter_dict(s2))
-
-    def isAnagram__python_defaultdict__comparing_keys(self, s1: str, s2: str) -> bool:
+    def isAnagram__python_defaultdict__compare_keys(self, s1: str, s2: str) -> bool:
         """
         Solves "valid anagram" problem...
         -   using Python's `collections.defaultdict` to build a custom counter dictionary
         -   by iteratively comparing the counts of keys
         """
 
-        from collections import defaultdict as DefaultDict
-
-        # Create a dict counting the elements of an iterable.
-        def counter_dict(iterable):
-
-            counter = DefaultDict(int)
-
-            for item in iterable:
-                counter[item] += 1
-
-            return counter
-
-        #---------------------------------------
-
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
-            return False
-
-        # Count the characters in `s1` and `s2`.
-        c1 = counter_dict(s1)
-        c2 = counter_dict(s2)
-
-        # If `c1` and `c2` don't have the same number of keys, then `s1` and `s2` can't be anagrams.
-        if len(c1) != len(c2):
-            return False
-
-        # Compare the counts of keys.
-        # -   Because `c1` and `c2` have the same number of keys,
-        #     they must share all letters (but not necessarily their counts)
-        #     or they have the same number of unique letters (but their letters are different).
-        for letter in c1.keys():
-            # Does `letter` occur in `s2`?
-            if letter in c2.keys():
-                # Does `letter` occur in `s1` and `s2` the same number of times?
-                if c1[letter] == c2[letter]:
-                    # 🥳 Move on to the next letter!
-                    continue
-
-            return False
-
-        # If we made it here, then `s1` and `s2` are anagrams!
-        return True
+        return self._solve_with(
+            counter=self._counter__python_defaultdict,
+            compare=self._compare__compare_keys,
+        )(s1, s2)
 
     def isAnagram__custom_counter__equals_operator(self, s1: str, s2: str) -> bool:
         """
@@ -154,58 +86,77 @@ class Solution:
         -   using Python's equals operator `==`
         """
 
-        # Create a dict counting the elements of an iterable.
-        def counter_dict(iterable):
+        return self._solve_with(
+            counter=self._counter__custom_counter,
+            compare=self._compare__equals_operator,
+        )(s1, s2)
 
-            counter = dict()
-
-            for item in iterable:
-                if item in counter:
-                    counter[item] += 1
-                else:
-                    counter[item] = 1
-
-            return counter
-
-        #---------------------------------------
-
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
-            return False
-
-        # And then it's just...
-        return (counter_dict(s1) == counter_dict(s2))
-
-    def isAnagram__custom_counter__comparing_keys(self, s1: str, s2: str) -> bool:
+    def isAnagram__custom_counter__compare_keys(self, s1: str, s2: str) -> bool:
         """
         Solves "valid anagram" problem...
         -   using a custom counter dictionary
         -   by iteratively comparing the counts of keys
         """
 
+        return self._solve_with(
+            counter=self._counter__custom_counter,
+            compare=self._compare__compare_keys,
+        )(s1, s2)
+
+    ########################################
+    #   Pieces
+    ########################################
+
+    def _solve_with(self, counter: _CounterFun, compare: _CompareFun) -> _SolutionFun:
+
+        def solve(s1: str, s2: str) -> bool:
+            # If `s1` and `s2` aren't the same length, then they can't be anagrams.
+            if len(s1) != len(s2):
+                return False
+
+            # Make counters.
+            c1 = counter(s1)
+            c2 = counter(s2)
+
+            # Test equality.
+            return compare(c1, c2)
+
+        return solve
+
+    def _counter__python_counter(self, iterable: Iterable) -> Counter:
         # Create a dict counting the elements of an iterable.
-        def counter_dict(iterable):
+        return Counter(iterable)
 
-            counter = dict()
+    def _counter__python_defaultdict(self, iterable: Iterable) -> DefaultDict:
+        # Create a dict counting the elements of an iterable.
+        counter = DefaultDict(int)
 
-            for item in iterable:
-                if item in counter:
-                    counter[item] += 1
-                else:
-                    counter[item] = 1
+        for item in iterable:
+            counter[item] += 1
 
-            return counter
+        return counter
 
-        #---------------------------------------
+    def _counter__custom_counter(self, iterable: Iterable) -> dict:
+        # Create a dict counting the elements of an iterable.
+        counter = dict()
 
-        # If `s1` and `s2` aren't the same length, then they can't be anagrams.
-        if len(s1) != len(s2):
+        for item in iterable:
+            if item in counter:
+                counter[item] += 1
+            else:
+                counter[item] = 1
+
+        return counter
+
+    def _compare__equals_operator(self, c1, c2) -> bool:
+        # If `c1` and `c2` don't have the same number of keys, then `s1` and `s2` can't be anagrams.
+        if len(c1) != len(c2):
             return False
 
-        # Count the characters in `s1` and `s2`.
-        c1 = counter_dict(s1)
-        c2 = counter_dict(s2)
+        # And then it's just...
+        return (c1 == c2)
 
+    def _compare__compare_keys(self, c1, c2) -> bool:
         # If `c1` and `c2` don't have the same number of keys, then `s1` and `s2` can't be anagrams.
         if len(c1) != len(c2):
             return False
@@ -281,7 +232,7 @@ class TestSolution(unittest.TestCase):
 
     def test_strings_of_different_lengths(self):
 
-        (n1, n2) = random.sample(range(2, 10), 2)
+        (n1, n2) = random.sample(range(10, 20), 2)
         s1 = self._random_string(n1)
         s2 = self._random_string(n2)
 
@@ -296,7 +247,7 @@ class TestSolution(unittest.TestCase):
 
     def test_random_matching_strings(self):
 
-        n = random.randrange(2, 10)
+        n = random.randrange(10, 20)
         s1 = self._random_string(n)
         s2 = self._shuffle_string(s1)
 
@@ -311,7 +262,7 @@ class TestSolution(unittest.TestCase):
 
     def test_random_nonmatching_strings(self):
 
-        n = random.randrange(2, 10)
+        n = random.randrange(10, 20)
         s1 = self._random_string(n, string.ascii_letters)
         s2 = self._random_string(n, string.digits)
 
