a2f67c2f384c3d20e282c8a3ece12bf287fa2340
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-02 18:13:25 -0400

    leetcode.com/climbing_stairs.py: setup

--- leetcode.com/climbing_stairs.py
+++ leetcode.com/climbing_stairs.py
@@ -1,1 +1,40 @@
 #!python3
+
+############################################################
+
+
+class Solution:
+
+    def climbStairs(self, n: int) -> int:
+
+        return 0
+
+
+############################################################
+
+############################################################
+
+import unittest    # noqa: E402
+
+
+class TestSolution(unittest.TestCase):
+
+    def _run_solution(self, args):
+        return Solution().climbStairs(*args)
+
+    def test_something(self):
+
+        args = tuple()
+        answer = None
+
+        result = self._run_solution(args)
+
+        self.assertEquals(result, answer)
+
+        return
+
+
+############################################################
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
