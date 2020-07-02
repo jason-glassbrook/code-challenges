717cc2820a6fc76c68461aacbcc6e88ede7652b3
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-02 18:37:03 -0400

    l~c~/c~s~.py: setup tests

--- leetcode.com/climbing_stairs.py
+++ leetcode.com/climbing_stairs.py
@@ -7,7 +7,7 @@ class Solution:
 
     def climbStairs(self, n: int) -> int:
 
-        return 0
+        return 1
 
 
 ############################################################
@@ -19,17 +19,36 @@ import unittest    # noqa: E402
 
 class TestSolution(unittest.TestCase):
 
+    EXAMPLES = [{
+        "args": [1],
+        "answer": 1,
+    }, {
+        "args": [2],
+        "answer": 2,
+    }, {
+        "args": [3],
+        "answer": 3,
+    }, {
+        "args": [4],
+        "answer": 5,
+    }, {
+        "args": [5],
+        "answer": 8,
+    }, {
+        "args": [6],
+        "answer": 13,
+    }]
+
     def _run_solution(self, args):
         return Solution().climbStairs(*args)
 
-    def test_something(self):
+    def test_examples(self):
 
-        args = tuple()
-        answer = None
+        for example in self.EXAMPLES:
 
-        result = self._run_solution(args)
+            result = self._run_solution(example["args"])
 
-        self.assertEquals(result, answer)
+            self.assertEqual(result, example["answer"])
 
         return
 
