b75cee38fe8be1a16c2f5c8b08831cd03806f65f
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-03 18:37:49 -0400

    leetcode/best_time_to_buy_and_sell_stock.py: setup

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -1,1 +1,30 @@
 #!python3
+
+############################################################
+
+from typing import List
+
+
+class Solution:
+
+    def maxProfit(self, prices: List[int]) -> int:
+
+        return 0
+
+
+############################################################
+
+import unittest    # noqa: E402
+
+
+class TestSolution(unittest.TestCase):
+
+    def _run_solution(self, args):
+
+        return Solution().maxProfit(*args)
+
+
+############################################################
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
