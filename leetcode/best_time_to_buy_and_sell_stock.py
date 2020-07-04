34f8036f53287b41f6c3a6aabefba1163624e3a6
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-03 22:26:30 -0400

    l~/best~stocks.py: def more tests

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -15,20 +15,25 @@ class Solution:
 ############################################################
 
 import unittest    # noqa: E402
+import random    # noqa: E402
 
 
 class TestSolution(unittest.TestCase):
 
-    def _run_solution(self, args):
+    _MIN_VALUE = 0
+    _MAX_VALUE = 100
+    _LENGTH = 10
+
+    def _run_solution(self, *args):
 
         return Solution().maxProfit(*args)
 
     def test_example_1(self):
 
-        args = [[7, 1, 5, 3, 6, 4]]
+        series = [7, 1, 5, 3, 6, 4]
         answer = 5
 
-        result = self._run_solution(args)
+        result = self._run_solution(series)
 
         self.assertEqual(result, answer)
 
@@ -36,21 +41,54 @@ class TestSolution(unittest.TestCase):
 
     def test_example_2(self):
 
-        args = [[7, 6, 4, 3, 1]]
+        series = [7, 6, 4, 3, 1]
         answer = 0
 
-        result = self._run_solution(args)
+        result = self._run_solution(series)
 
         self.assertEqual(result, answer)
 
         return
 
-    def test_empty_list(self):
+    def test_empty_series(self):
 
-        args = [[]]
+        series = []
         answer = 0
 
-        result = self._run_solution(args)
+        result = self._run_solution(series)
+
+        self.assertEqual(result, answer)
+
+        return
+
+    def test_flat_series(self):
+
+        series = [random.randint(self._MIN_VALUE, self._MAX_VALUE)] * self._LENGTH
+        answer = 0
+
+        result = self._run_solution([series])
+
+        self.assertEqual(result, answer)
+
+        return
+
+    def test_all_ascending_series(self):
+
+        series = list(range(self._MIN_VALUE, self._MAX_VALUE + 1, +1))
+        answer = max(0, max(series) - min(series))
+
+        result = self._run_solution(series)
+
+        self.assertEqual(result, answer)
+
+        return
+
+    def test_all_descending_series(self):
+
+        series = list(range(self._MAX_VALUE, self._MIN_VALUE - 1, -1))
+        answer = max(0, min(series) - max(series))
+
+        result = self._run_solution(series)
 
         self.assertEqual(result, answer)
 
