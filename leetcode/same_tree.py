b0fd4e1b1d445967314eb9125eb71b79a462300e
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-07 00:39:00 -0400

    l~/same_tree.py: deserialize tree data for tests

--- leetcode/same_tree.py
+++ leetcode/same_tree.py
@@ -2,26 +2,53 @@
 
 ############################################################
 
+from typing import Optional, Iterable
+
 
 # Definition for a binary tree node.
 class TreeNode:
 
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
+    def __init__(self, value=0, left=None, right=None):
+
+        self.value = value
         self.left = left
         self.right = right
+
+        return
+
+
+# Deserialize a tree.
+def tree_from_data(data: Optional[Iterable]):
+    """
+    Deserialize a binary tree from `data`, which should be of the form `[n, l, r]` or `None`,
+    -   where `n` is a scalar value,
+    -   where `l` and `r` may be `[n, l, r]`, scalar, or `None`.
+    """
+
+    def branch(value, left, right):
+
+        return TreeNode(
+            value,
+            branch(*left) if isinstance(left, Iterable) else left,
+            branch(*right) if isinstance(right, Iterable) else right,
+        )
+
+    return branch(*data) if isinstance(data, Iterable) else None
+
+
+############################################################
 
 
 class Solution:
 
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+
         return True
 
 
 ############################################################
 
 import unittest    # noqa: E402
-import random    # noqa: E402
 
 
 class TestSolution(unittest.TestCase):
@@ -30,45 +57,47 @@ class TestSolution(unittest.TestCase):
 
         return Solution().isSameTree(*args)
 
-    def test_example_1(self):
+    def _run_example(self, args, answer):
 
-        trees = [
-            [1, 2, 3],
-            [1, 2, 3],
-        ]
-        answer = True
-
-        result = self._run_solution(*trees)
+        result = self._run_solution(*args)
 
         self.assertEqual(result, answer)
+
+        return
+
+    def test_example_1(self):
+
+        self._run_example(
+            args=[
+                tree_from_data([1, 2, 3]),
+                tree_from_data([1, 2, 3]),
+            ],
+            answer=True,
+        )
 
         return
 
     def test_example_2(self):
 
-        trees = [
-            [1, 2, None],
-            [1, None, 2],
-        ]
-        answer = True
-
-        result = self._run_solution(*trees)
-
-        self.assertEqual(result, answer)
+        self._run_example(
+            args=[
+                tree_from_data([1, 2, None]),
+                tree_from_data([1, None, 2]),
+            ],
+            answer=False,
+        )
 
         return
 
     def test_example_3(self):
 
-        trees = [
-            [1, 2, 1],
-            [1, 1, 2],
-        ]
-        answer = True
-
-        result = self._run_solution(*trees)
-
-        self.assertEqual(result, answer)
+        self._run_example(
+            args=[
+                tree_from_data([1, 2, 1]),
+                tree_from_data([1, 1, 2]),
+            ],
+            answer=False,
+        )
 
         return
 
