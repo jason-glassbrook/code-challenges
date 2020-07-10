5a5eee4bce67bc2482c65eb3fb65986aa38e3397
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-09 21:59:01 -0400

    leetcode/lowest_common~tree.py: setup

--- leetcode/lowest_common_ancestor_of_a_binary_search_tree.py
+++ leetcode/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -1,1 +1,34 @@
 #!python3
+
+############################################################
+
+from leetcode.tools.binary_tree import TreeNode
+
+
+class Solution:
+
+    MAIN = "lowestCommonAncestor"
+
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+
+        pass
+
+
+############################################################
+
+import unittest    # noqa: E402
+
+from leetcode.tools import testing    # noqa: E402
+from leetcode.tools.binary_tree import tree_from_data    # noqa: E402
+
+
+class TestSolution(testing.TestSolution):
+
+    SOLUTION_CLASS = Solution
+    SOLUTION_FUNCTION = Solution.MAIN
+
+
+############################################################
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
