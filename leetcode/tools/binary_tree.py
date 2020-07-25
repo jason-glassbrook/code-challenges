1333af60aa2b36513845dddd6d6fb96a52ac7163
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-25 17:15:26 -0400

    l~/tools/binary_tree.py: fix to pass spec

--- leetcode/tools/binary_tree.py
+++ leetcode/tools/binary_tree.py
@@ -23,20 +23,51 @@ class TreeNode:
 
 
 # Deserialize a binary tree.
-def tree_from_data(data: Optional[Iterable]):
+def tree_from_data(data: Optional[Iterable]) -> Optional[TreeNode]:
     """
     Deserialize a binary tree from `data`, which should be of the form `[n, l, r]` or `None`,
     -   where `n` is a scalar value,
     -   where `l` and `r` may be `[n, l, r]`, scalar, or `None`.
     """
 
-    def convert_data_to_tree(val, left, right):
+    def convert_scalar_data(scalar_data) -> Optional[TreeNode]:
 
-        return TreeNode(
-            val,
-            convert_data_to_tree(*left) if isinstance(left, Iterable) else TreeNode(left),
-            convert_data_to_tree(*right)
-            if isinstance(right, Iterable) else TreeNode(right),
-        )
+        if scalar_data is None:
+            return None
 
-    return convert_data_to_tree(*data) if data and isinstance(data, Iterable) else None
+        return TreeNode(val=scalar_data)
+
+    def convert_list_data(list_data: Iterable) -> Optional[TreeNode]:
+
+        n = len(list_data)
+        node = None
+
+        if n == 0:
+            return node
+
+        if n >= 1:
+            node = convert_scalar_data(list_data[0])
+
+        if node is None:
+            return node
+
+        if n >= 2:
+            node.left = convert_data(list_data[1])
+
+        if n >= 3:
+            node.right = convert_data(list_data[2])
+
+        if n > 3:
+            print("[binary_tree.tree_from_data] Warning: Ignoring extra child nodes.")
+
+        return node
+
+    def convert_data(data) -> Optional[TreeNode]:
+
+        if isinstance(data, Iterable):
+            return convert_list_data(data)
+
+        else:
+            return convert_scalar_data(data)
+
+    return convert_data(data)
