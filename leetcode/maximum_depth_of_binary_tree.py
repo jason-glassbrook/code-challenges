63cdb84259f142bce55c1c29af332c18288962be
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-27 14:36:47 -0400

    l~/max~tree.py: solve with recursive drilling

--- leetcode/maximum_depth_of_binary_tree.py
+++ leetcode/maximum_depth_of_binary_tree.py
@@ -11,7 +11,7 @@ class Solution:
 
     def maxDepth(self, root: TreeNode) -> int:
 
-        return self.maxDepth__recursive__adding(root)
+        return self.maxDepth__recursive__drilling(root)
 
     def maxDepth__recursive__adding(self, root: TreeNode) -> int:
 
@@ -27,6 +27,21 @@ class Solution:
             return depth_of_branch
 
         return get_depth_of_branch(root)
+
+    def maxDepth__recursive__drilling(self, root: TreeNode) -> int:
+
+        def get_depth_of_branch(node: TreeNode, depth: int = 0) -> int:
+
+            if node is None:
+                return depth
+
+            depth_of_left = get_depth_of_branch(node.left, depth)
+            depth_of_right = get_depth_of_branch(node.right, depth)
+            depth_of_branch = 1 + max(depth_of_left, depth_of_right)
+
+            return depth_of_branch
+
+        return get_depth_of_branch(root, 0)
 
 
 ############################################################
