c1698be6d6a22a4e373dd4d4a786c11a3fa44a84
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-07-27 20:25:57 -0400

    l~/t~/testing.py: redef for more complex tests

--- leetcode/tools/testing.py
+++ leetcode/tools/testing.py
@@ -2,9 +2,22 @@
 
 ############################################################
 
+from typing import (
+    Any,
+    Type,
+    Callable,
+    Iterable,
+    Union,
+    Optional,
+)
 import unittest
 
 ############################################################
+
+
+def getattr_of_instance(some_class: Type, attr_name: str) -> Any:
+
+    return getattr(some_class(), attr_name)
 
 
 class TestSolution(unittest.TestCase):
@@ -12,18 +25,62 @@ class TestSolution(unittest.TestCase):
     SOLUTION_CLASS = None
     SOLUTION_FUNCTION = None
 
-    def run_solution(self, *args):
+    def run_solution(self, *args: Iterable) -> Any:
 
-        solution = self.SOLUTION_CLASS()
-        function = getattr(solution, self.SOLUTION_FUNCTION)
+        function = getattr_of_instance(
+            self.SOLUTION_CLASS,
+            self.SOLUTION_FUNCTION,
+        )
 
         return function(*args)
 
-    def run_test(self, args, answer, assertion="assertEqual"):
+    def run_test(
+        self,
+        args: Iterable,
+        answer: Any,
+        assertion: Union[Callable, str, None] = None,
+        assertion__rest: Optional[Iterable] = None,
+        transform_result: Optional[Callable] = None,
+        transform_result__rest: Optional[Iterable] = None,
+        transform_answer: Optional[Callable] = None,
+        transform_answer__rest: Optional[Iterable] = None,
+    ) -> Any:
 
-        result = self.run_solution(*args)
-        compare = getattr(self, assertion)
+        #---------------------------------------
+        #   Defaults
+        #---------------------------------------
 
-        compare(result, answer)
+        if assertion is None:
+            assertion = "assertEqual"
 
-        return
+        if isinstance(assertion, str):
+            assertion = getattr(self, assertion)
+
+        if assertion__rest is None:
+            assertion__rest = tuple()
+
+        if transform_result is None:
+            transform_result = lambda *args: args[0]    # noqa: E731
+
+        if transform_result__rest is None:
+            transform_result__rest = tuple()
+
+        if transform_answer is None:
+            transform_answer = lambda *args: args[0]    # noqa: E731
+
+        if transform_answer__rest is None:
+            transform_answer__rest = tuple()
+
+        #---------------------------------------
+
+        result = transform_result(
+            self.run_solution(*args),
+            *transform_result__rest,
+        )
+
+        answer = transform_answer(
+            answer,
+            *transform_answer__rest,
+        )
+
+        return assertion(result, answer, *assertion__rest)
