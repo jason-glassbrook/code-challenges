ba6f60a6485a1e8850b1fa44a3c385f39348beb0
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-08-07 15:06:46 -0400

    l~/lowest~tree.py: update tests to correctly handle data types

--- leetcode/lowest_common_ancestor_of_a_binary_search_tree.py
+++ leetcode/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -20,6 +20,8 @@ class Solution:
 
 import unittest    # noqa: E402
 
+from typing import Any    # noqa: E402
+
 from leetcode.tools import testing    # noqa: E402
 from leetcode.tools.binary_tree import tree_from_data    # noqa: E402
 
@@ -31,11 +33,33 @@ class TestSolution(testing.TestSolution):
     SOLUTION_CLASS = Solution
     SOLUTION_FUNCTION = Solution.MAIN
 
-    def test_example_1(self):
+    def get_val(self, node: Union[TreeNode, None]) -> Any:
+
+        return (node.val if node else None)
+
+    def run_test__is_same_node(
+        self,
+        args: Any,
+        answer: Any,
+    ):
+
+        (tree_data, p_data, q_data) = args
+
+        root = tree_from_data(tree_data)
+        p = TreeNode(p_data) if p_data else None
+        q = TreeNode(q_data) if q_data else None
 
         return self.run_test(
+            args=[root, p, q],
+            transform_result=self.get_val,
+            answer=answer,
+        )
+
+    def test_example_1(self):
+
+        return self.run_test__is_same_node(
             args=[
-                tree_from_data([6, [2, 0, [4, 3, 5]], [8, 7, 9]]),
+                [6, [2, 0, [4, 3, 5]], [8, 7, 9]],
                 2,
                 8,
             ],
@@ -44,9 +68,9 @@ class TestSolution(testing.TestSolution):
 
     def test_example_2(self):
 
-        return self.run_test(
+        return self.run_test__is_same_node(
             args=[
-                tree_from_data([6, [2, 0, [4, 3, 5]], [8, 7, 9]]),
+                [6, [2, 0, [4, 3, 5]], [8, 7, 9]],
                 2,
                 4,
             ],
@@ -55,9 +79,9 @@ class TestSolution(testing.TestSolution):
 
     def test_empty_tree(self):
 
-        return self.run_test(
+        return self.run_test__is_same_node(
             args=[
-                tree_from_data([]),
+                [],
                 1,
                 2,
             ],
@@ -66,20 +90,20 @@ class TestSolution(testing.TestSolution):
 
     def test_tree_of_depth_1(self):
 
-        return self.run_test(
+        return self.run_test__is_same_node(
             args=[
-                tree_from_data([1]),
+                [1],
                 1,
-                2,
+                None,
             ],
             answer=None,
         )
 
     def test_tree_of_depth_2(self):
 
-        return self.run_test(
+        return self.run_test__is_same_node(
             args=[
-                tree_from_data([1, 2, None]),
+                [1, 2, None],
                 1,
                 2,
             ],
