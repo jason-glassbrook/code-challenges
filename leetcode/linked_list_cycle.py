65e53fee1466018b57bffd3a18acdcc95e35e674
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-10-14 23:25:28 -0400

    l~/linked_list_cycle.py: boilerplate

--- leetcode/linked_list_cycle.py
+++ leetcode/linked_list_cycle.py
@@ -1,1 +1,41 @@
 #!python3
+
+############################################################
+
+from typing import Union
+
+from leetcode.tools.singly_linked_list import ListNode
+
+MaybeListNode = Union[ListNode, None]
+
+#-----------------------------------------------------------
+
+
+class Solution:
+
+    MAIN = "hasCycle"
+
+    def hasCycle(self, head: ListNode) -> bool:
+
+        pass
+
+
+############################################################
+
+import unittest    # noqa: E402
+
+from leetcode.tools import testing    # noqa: E402
+
+#-----------------------------------------------------------
+
+
+class TestSolution(testing.TestSolution):
+
+    SOLUTION_CLASS = Solution
+    SOLUTION_FUNCTION = Solution.MAIN
+
+
+############################################################
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
