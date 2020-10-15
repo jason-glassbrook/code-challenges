5a8cb972807d2cd119a62b1dc6a3063f976af9f5
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-10-15 16:47:11 -0400

    l~/s~l~list.py: def serializer/de~ for nodes

--- leetcode/tools/singly_linked_list.py
+++ leetcode/tools/singly_linked_list.py
@@ -1,7 +1,16 @@
 #!python3
 
+############################################################
 
-# Definition for singly-linked list node.
+from typing import (
+    Any,
+    Union,
+)
+
+#-----------------------------------------------------------
+
+
+# Definition for a singly-linked list node.
 class ListNode:
 
     def __init__(self, val=0, next=None):
@@ -10,3 +19,32 @@ class ListNode:
         self.next = next
 
         return
+
+
+#-----------------------------------------------------------
+
+MaybeListNode = Union[ListNode, None]
+
+
+# Deserialize a singly-linked list node.
+def node_from_data(data: Any) -> MaybeListNode:
+    """
+    Deserialize a singly-linked list node from `data`, which should be something or `None`.
+    """
+
+    if data is None:
+        return None
+
+    return ListNode(val=data)
+
+
+# Serialize a singly-linked list node.
+def data_from_node(node: MaybeListNode) -> Any:
+    """
+    Serialize data from a singly-linked list `node`, which should be a `ListNode` or `None`.
+    """
+
+    if node is None:
+        return None
+
+    return node.val
