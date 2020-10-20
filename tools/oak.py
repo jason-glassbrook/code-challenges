4ae432b2938ed142dfb4551a2f989085f6ed8a0b
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-10-20 15:23:42 -0400

    t~/oak.py: spec lots of type-checking funs... -- as comments

--- tools/oak.py
+++ tools/oak.py
@@ -7,11 +7,14 @@
 ############################################################
 
 import sys as _sys
+# import collections as _collections
 from collections import abc as _abc
 
 from . import twine as _twine
 from . import decor as _decor
 
+#-----------------------------------------------------------
+#   DECORATORS
 #-----------------------------------------------------------
 
 _DLI_FUN_TYPE = _abc.Callable[..., bool]
@@ -92,3 +95,108 @@ def def_logical_inverse(
 
     # And we're done!
     return fun
+
+#-----------------------------------------------------------
+#   TYPE-CHECKING FUNCTIONS
+#-----------------------------------------------------------
+
+# @def_logical_inverse
+# def is_of(x, kinds) -> bool: return isinstance(x, kinds)
+
+# @def_logical_inverse
+# def is_none(x) -> bool: return x is None
+
+#---------------------------------------
+#   from `builtins`
+#---------------------------------------
+
+# @def_logical_inverse
+# def is_bool(x) -> bool: return is_of(x, bool)
+
+# @def_logical_inverse
+# def is_bytearray(x) -> bool: return is_of(x, bytearray)
+
+# @def_logical_inverse
+# def is_bytes(x) -> bool: return is_of(x, bytes)
+
+# @def_logical_inverse
+# def is_complex(x) -> bool: return is_of(x, complex)
+
+# @def_logical_inverse
+# def is_dict(x) -> bool: return is_of(x, dict)
+
+# @def_logical_inverse
+# def is_float(x) -> bool: return is_of(x, float)
+
+# @def_logical_inverse
+# def is_frozenset(x) -> bool: return is_of(x, frozenset)
+
+# @def_logical_inverse
+# def is_int(x) -> bool: return is_of(x, int)
+
+# @def_logical_inverse
+# def is_list(x) -> bool: return is_of(x, list)
+
+# @def_logical_inverse
+# def is_property(x) -> bool: return is_of(x, property)
+
+# @def_logical_inverse
+# def is_range(x) -> bool: return is_of(x, range)
+
+# @def_logical_inverse
+# def is_set(x) -> bool: return is_of(x, set)
+
+# @def_logical_inverse
+# def is_slice(x) -> bool: return is_of(x, slice)
+
+# @def_logical_inverse
+# def is_str(x) -> bool: return is_of(x, str)
+
+# @def_logical_inverse
+# def is_tuple(x) -> bool: return is_of(x, tuple)
+
+# @def_logical_inverse
+# def is_type(x) -> bool: return is_of(x, type) or str(type(x)).startswith("<class 'typing.")
+
+#---------------------------------------
+#   from `collections`
+#---------------------------------------
+
+# collections.deque
+# collections.ChainMap
+# collections.Counter
+# collections.OrderedDict
+# collections.defaultdict
+# collections.UserDict
+# collections.UserList
+# collections.UserString
+
+#---------------------------------------
+#   from `collections.abc`
+#---------------------------------------
+
+# collections.abc.Container
+# collections.abc.Hashable
+# collections.abc.Iterable
+# collections.abc.Iterator
+# collections.abc.Reversible
+# collections.abc.Generator
+# collections.abc.Sized
+# collections.abc.Callable
+# collections.abc.Collection
+# collections.abc.Sequence
+# collections.abc.MutableSequence
+# collections.abc.ByteString
+# collections.abc.Set
+# collections.abc.MutableSet
+# collections.abc.Mapping
+# collections.abc.MutableMapping
+# collections.abc.MappingView
+# collections.abc.ItemsView
+# collections.abc.KeysView
+# collections.abc.ValuesView
+# collections.abc.Awaitable
+# collections.abc.Coroutine
+# collections.abc.AsyncIterable
+# collections.abc.AsyncIterator
+# collections.abc.AsyncGenerator
