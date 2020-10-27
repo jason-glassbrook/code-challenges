6a152ef88d229493d2143f74a0743f044f7a3ea2
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2020-10-26 23:59:53 -0400

    l~/t~/s~l~list.py: def list_from_node

--- leetcode/tools/singly_linked_list.py
+++ leetcode/tools/singly_linked_list.py
@@ -7,6 +7,8 @@ from typing import (
     Union,
 )
 
+from tools import oak as _oak
+
 #-----------------------------------------------------------
 
 
@@ -23,6 +25,7 @@ class ListNode:
 
 #-----------------------------------------------------------
 
+MaybeIterable = Union[Iterable, None]
 MaybeListNode = Union[ListNode, None]
 
 
@@ -48,3 +51,49 @@ def data_from_node(node: MaybeListNode) -> Any:
         return None
 
     return node.val
+
+
+# Deserialize a singly-linked list.
+def list_from_data(data: MaybeIterable) -> MaybeListNode:
+    """
+    Deserialize a singly-linked list from `data`, which must be a `list` or `None`.
+    If `data` is a list, then it must be a list of items, where each item is...
+    -   a `dict` like `{ "val": Any, "next_index": int, ... }`,
+        where `next_index` is the index the item's `next` `ListNode` in `data`;
+    -   a `dict` like `{ "val": Any, ... }`;
+    -   or something else.
+    """
+
+    if data is None:
+        return None
+
+    if not _oak.is_Iterable(data):
+        raise TypeError("The provided `data` must be `Iterable` or `None`.")
+
+    n = len(data)
+    node_list = [None] * n
+    next_list = [None] * n
+
+    # Process `data` list into `node_list` and `next_list`.
+    for (i, item) in enumerate(data):
+
+        if _oak.is_dict(item):
+            # If `item` is like `{ "val": Any, ... }`...
+            if "val" in item:
+                node_list[i] = ListNode(val=item["val"])
+
+                # If `item` is like `{ "val": Any, "next_index": int, ... }`...
+                if "next_index" in item:
+                    next_list[i] = item["next_index"]
+
+        else:
+            node_list[i] = ListNode(val=item)
+
+    # Link up nodes based on `next_list`.
+    for (i, next_index) in enumerate(next_list):
+        if next_index:
+            node_list[i].next = node_list[next_index]
+        elif i < (n - 1):
+            node_list[i].next = node_list[i + 1]
+
+    return node_list[0]
